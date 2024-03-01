9111c9863eab71b1597047d56df4ffd773165454
simo448g <sjohan19@student.aau.dk>
2024-03-01 10:15:23 +0100

    test

---
+++ Simulink Version/Run_simulation.m
@@ -0,0 +1,30 @@
+%% Describtion
+% This short script is utilized to run the simulink simulation  
+%% Making alot of clears 
+clf 
+clc 
+clear
+%% Adding path and standard values
+addpath('..\Functions\')
+addpath('..\')
+c=standardConstants; 
+%% 
+c.Tsim='48*3600'; 
+c.tsSim=num2str(c.ts*3600); 
+
+simData=sim('Global_MPC.slx',"StartTime",'0',"StopTime",c.Tsim,'FixedStep',c.tsSim);
+
+%% 
+clf
+%Makign a plot of the volume
+V=simData.logsout{2}.Values.Data;
+hold on 
+plot(V)
+yline(c.hmax*c.At)
+yline(c.hmin*c.At)
+hold off 
+ylabel('Volume [m^{3}]')
+xlabel('Samples [*]')
+grid on
+ylim([220 600])
+legend('Volume','Constraints')
